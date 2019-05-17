99b0cf546cb053e2417de9c43d8d02f2e691ae91
BiggaHD <dzenis.hankusic@gmail.com>
2019-05-18 01:44:27 +0200

    almostDone :)

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,31 +1,31 @@
 // Select DOM Items
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-item');
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
 
 // Set Initial State Of Menu
 let showMenu = false;
 
-menuBtn.addEventListener('click', toggleMenu);
+menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
   if (!showMenu) {
-    menuBtn.classList.add('close');
-    menu.classList.add('show');
-    menuNav.classList.add('show');
-    menuBranding.classList.add('show');
-    navItems.forEach(item => item.classList.add('show'));
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
 
     // Set Menu State
     showMenu = true;
   } else {
-    menuBtn.classList.remove('close');
-    menu.classList.remove('show');
-    menuNav.classList.remove('show');
-    menuBranding.classList.remove('show');
-    navItems.forEach(item => item.classList.remove('show'));
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
 
     // Set Menu State
     showMenu = false;
@@ -33,13 +33,19 @@ function toggleMenu() {
 }
 
 function strReplace() {
-    htmlContent = menuBtn.innerHTML;
-    textContent = menuBtn.textContent.replace(/\s/g, "");
-    let val = ''
-    if (textContent === 'MORE')  {
-      val = 'Close';
-    } else {
-      val = 'MORE';
-    }
-    return val;
-  };
+  htmlContent = menuBtn.innerHTML;
+  textContent = menuBtn.textContent.replace(/\s/g, "");
+  let val = "";
+  if (textContent === "MORE") {
+    val = "Close";
+  } else {
+    val = "MORE";
+  }
+  return val;
+}
+
+// Cloning divs where particles go in order not to put 300 of them in the markup :)
+const node = document.querySelector(".parts");
+[...Array(300)].forEach(_ =>
+  node.parentNode.insertBefore(node.cloneNode(true), node)
+);
